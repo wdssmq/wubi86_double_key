8d86560842a7b7b30e78dbe980aac4e41ff1e8c9
wdssmq <wdssmq@qq.com>
2026-08-01 00:18:03 +0800

    up. 修改用户字典名称并更新推送脚本

--- trime.bat
+++ trime.bat
@@ -1,6 +1,7 @@
 adb push trime.custom.yaml /sdcard/rime
 adb push wubi86_double_key.schema.yaml /sdcard/rime
 adb push wubi86_double_key.dict.yaml /sdcard/rime
+adb push custom_dicts /sdcard/rime
 
 adb shell am broadcast -a com.osfans.trime.deploy
 pause
